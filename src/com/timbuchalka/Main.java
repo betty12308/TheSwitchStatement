a84b885b4203eefaa262a5c7a552345adc124afe
betty_guo22 <elizabethpguo@gmail.com>
2022-05-26 18:38:36 -0400

    Initial commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,66 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 3;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+        int switchValue = 3;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+
+        }
+
+        char charValue = 'A';
+        switch (charValue) {
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println(charValue + " was found");
+                break;
+
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break;
+        }
+
+        String month = "January";
+        switch(month) {
+            case "January":
+                System.out.println("Jan");
+                break;
+            case "June":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+        //More code here
+    }
+}
